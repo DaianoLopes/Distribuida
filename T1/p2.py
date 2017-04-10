95933c8ae2e8c40d52e39e6812503cbca471be84
DaianoLopes <daiano.lopes@hotmail.com>
2017-04-10 08:41:31 -0300

    Atualização T1 - Finalizado

--- T1/p2.py
+++ T1/p2.py
@@ -1,13 +1,13 @@
 from bottle import run, get, post, request, redirect
 
-from datetime import datetime
 historico = []
 
 @get('/home')
 def index():
     form = '''
+        <h3>Bem vindo ao Chat, digite seu nome e mensagem a baixo:</h3>
 	<form action="/send" method="post">
-		Nome:<br><input type="text" name="nome"><br>
+		Nome:<br><input type="text" name="name"><br>
 		Mensagem:<br><input type="text" name="mensagem">
 		<input value="Enviar" type="submit"/>
 	</form>
@@ -18,7 +18,7 @@ def index():
 
 @post('/send')
 def index2():
-    nome = request.forms.get('nome')
+    nome = request.forms.get('name')
     mensagem = request.forms.get('mensagem')
     historico.append([nome, mensagem])
     redirect('/home')
